e5459dd18729c9740878e5b3f1fcec49578d22e8
hyouness <youness.haidar.01@gmail.com>
2019-08-13 05:13:53 +0300

    Implement JSON parsing functionality

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,59 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+        if (!json.isEmpty()) {
+            Sandwich sandwich = new Sandwich();
+            try {
+                JSONObject sandwichObject = new JSONObject(json);
+
+                // Parse Name fields
+                JSONObject sandwichNameObject = sandwichObject.getJSONObject("name");
+                String mainName = sandwichNameObject.getString("mainName");
+                sandwich.setMainName(mainName);
+                JSONArray sandwichNameArray = sandwichNameObject.getJSONArray("alsoKnownAs");
+                List<String> commonNames = new ArrayList<>();
+                parseJSONArray(sandwichNameArray, commonNames);
+                sandwich.setAlsoKnownAs(commonNames);
+
+                // Parse Place of Origin field
+                String placeOfOrigin = sandwichObject.getString("placeOfOrigin");
+                sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+                // Parse Description field
+                String description = sandwichObject.getString("description");
+                sandwich.setDescription(description);
+
+                // Parse Image Field
+                String image = sandwichObject.getString("image");
+                sandwich.setImage(image);
+
+                // Parse Ingredients field
+                JSONArray ingredientsJSONArray = sandwichObject.getJSONArray("ingredients");
+                List<String> ingredients = new ArrayList<>();
+                parseJSONArray(ingredientsJSONArray, ingredients);
+                sandwich.setIngredients(ingredients);
+
+                return sandwich;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
+    }
+
+    private static void parseJSONArray(JSONArray jsonArray, List<String> list) throws JSONException {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            list.add(jsonArray.getString(i));
+        }
     }
 }
